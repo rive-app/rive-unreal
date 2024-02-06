43bb3dfd115ca73f685c97d1bc565c13e7840af9
Batname <dadubinin@gmail.com>
2024-02-06 11:17:04 +0100

    add to garbage

--- Source/Rive/Private/Game/RiveActor.cpp
+++ Source/Rive/Private/Game/RiveActor.cpp
@@ -13,9 +13,10 @@ ARiveActor::ARiveActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent0"));
-	
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent0"));	
 	check(RootComponent);
+
+	RootComponent->Mobility = EComponentMobility::Movable;
 	
 	ScreenUserWidget = CreateDefaultSubobject<URiveFullScreenUserWidget>(TEXT("ScreenUserWidget"));
 	
