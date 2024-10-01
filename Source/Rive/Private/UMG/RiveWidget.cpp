1384a03a468a555c0276d5b97549894e31295181
Ryan M. Shetley <ryan.shetley@gmail.com>
2024-10-01 09:05:53 -0500

    Check for world for Widgets; drag and dropping widgets in the UMG editor can have a World as null

--- Source/Rive/Private/UMG/RiveWidget.cpp
+++ Source/Rive/Private/UMG/RiveWidget.cpp
@@ -109,11 +109,15 @@ TSharedRef<SWidget> URiveWidget::RebuildWidget()
 	{
 		RiveTextureObject = NewObject<URiveTextureObject>();
 		RiveTextureObject->Size = FIntPoint::ZeroValue;  // Setting to zero value here will make the rive texture use the artboard size initially
-		TimerHandle.Invalidate();
-		GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this]()
+
+		if (UWorld* World = GetWorld())
 		{
-			Setup();
-		}, 0.05f, false);
+			World->GetTimerManager().ClearTimer(TimerHandle);
+			World->GetTimerManager().SetTimer(TimerHandle, [this]()
+			{
+				Setup();
+			}, 0.05f, false);
+		}
 	}
 	
 	return RiveWidget.ToSharedRef();
