3f0b0d2f9153923c9f03d41ee988ddb10bc54189
Tod-Rive <hello@rive.app>
2025-02-20 18:23:56 +0000

    fix: added rhi check for unsupported platforms
    On startup, the Rive plugin will now disable the RHI tech preview if on Mac platforms or UE5.3 on Windows.
    
    Diffs=
    479609dac1 fix: added rhi check for unsupported platforms (#9073)
    
    Co-authored-by: Tod-Rive <tod@rive.app>

--- Source/RiveRenderer/Private/RiveRendererSettings.cpp
+++ Source/RiveRenderer/Private/RiveRendererSettings.cpp
@@ -1,5 +1,34 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Copyright Rive, Inc. All rights reserved.
 
 #include "RiveRendererSettings.h"
 
 URiveRendererSettings::URiveRendererSettings() : bEnableRHITechPreview(false) {}
+
+#if WITH_EDITOR
+void URiveRendererSettings::PostInitProperties()
+{
+    Super::PostInitProperties();
+
+    if (!bCanUseRHI)
+    {
+        bEnableRHITechPreview = false;
+        SaveConfig();
+    }
+}
+
+bool URiveRendererSettings::CanEditChange(const FProperty* InProperty) const
+{
+    bool bParentValue = Super::CanEditChange(InProperty);
+
+    if (InProperty && InProperty->GetFName() ==
+                          GET_MEMBER_NAME_CHECKED(URiveRendererSettings,
+                                                  bEnableRHITechPreview))
+    {
+        // Only allow editing if running on Windows AND UE 5.4+
+        return bParentValue && bCanUseRHI;
+    }
+
+    return bParentValue;
+}
+
+#endif
--- Source/RiveRenderer/Public/RiveRendererSettings.h
+++ Source/RiveRenderer/Public/RiveRendererSettings.h
@@ -6,27 +6,35 @@
 #include "Engine/DeveloperSettings.h"
 #include "RiveRendererSettings.generated.h"
 
-/**
- *
- */
 UCLASS(Config = Engine, DefaultConfig)
 class RIVERENDERER_API URiveRendererSettings : public UDeveloperSettings
 {
     GENERATED_BODY()
 public:
-    /**
-     *
-     */
     URiveRendererSettings();
 
     UPROPERTY(EditAnywhere,
               config,
               Category = "Rive Experimental Settings",
-              DisplayName = "Enable RHI Technical Preview")
+              DisplayName = "Enable RHI Technical Preview",
+              META = (Tooltip = "Not available on Apple platforms or UE 5.3."))
     bool bEnableRHITechPreview;
 
     virtual FName GetCategoryName() const override
     {
         return FName(TEXT("Rive"));
     }
+
+#if WITH_EDITOR
+    virtual void PostInitProperties() override;
+    virtual bool CanEditChange(const FProperty* InProperty) const override;
+#endif
+
+private:
+#if PLATFORM_APPLE || ENGINE_MAJOR_VERSION < 5 ||                              \
+    (ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION < 4)
+    bool bCanUseRHI = false;
+#else
+    bool bCanUseRHI = true;
+#endif
 };
