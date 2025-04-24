7d11d01b3a371cbac9223a359087588e4b00eb91
luigi-rosso <hello@rive.app>
2025-04-24 17:15:01 +0000

    chore: format RiveWidget.cpp (#9532) c764be82aa
    Co-authored-by: Luigi Rosso <luigi-rosso@users.noreply.github.com>

--- Source/Rive/Private/UMG/RiveWidget.cpp
+++ Source/Rive/Private/UMG/RiveWidget.cpp
@@ -144,8 +144,11 @@ TSharedRef<SWidget> URiveWidget::RebuildWidget()
         if (UWorld* World = GetWorld())
         {
             World->GetTimerManager().ClearTimer(TimerHandle);
-            World->GetTimerManager()
-                .SetTimer(TimerHandle, [this]() { Setup(); }, 0.05f, false);
+            World->GetTimerManager().SetTimer(
+                TimerHandle,
+                [this]() { Setup(); },
+                0.05f,
+                false);
         }
     }
 
